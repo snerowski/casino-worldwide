0fe67a1ee35195484308c47cd4266b073e20548b
p0ptartz <ericwm420@gmail.com>
2023-01-13 10:37:40 -0500

    added for loop to create a 52 card deck in JS

--- high-low/hlassets/highlow.js
+++ high-low/hlassets/highlow.js
@@ -1,1 +1,21 @@
-console.log("hello")
+var suits = ["H", "S", "C", "D"]
+var rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",]
+var deck = []
+
+// nested for loop to combine all the suits to their values
+for (var i = 0; i < 4; i++) {
+    // console.log(suits[i])
+    for (var j = 0; j < 13; j++) {
+        // console.log(value[j])
+        // console.log(value[j] + suits[i])
+        // deck.push(rank[j] + suits[i])
+        deck.push({ suit: suits[i], rank: rank[j] });
+    }
+}
+console.log(deck)
+
+function shuffle() {
+
+}
+
+
